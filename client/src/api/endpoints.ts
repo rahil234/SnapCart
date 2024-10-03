9f37c2779c5161485306ea170b4e510e92ace3f7
Rahil <rahilsardar234@gmail.com>
2024-10-03 15:35:48 +0530

    test env

--- client/src/api/endpoints.ts
+++ client/src/api/endpoints.ts
@@ -1,7 +1,7 @@
 import axiosInstance from './axiosInstance';
 
 export const fetchItems = () => {
-  return axiosInstance.get('/api/user');
+  return axiosInstance.get('/api/uxcdhkegf');
 }; 
 
 export const createItem = (data : object) => {
