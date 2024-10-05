8ecf98c393ede037d2b03203d0b26414170efa00
Rahil <rahilsardar234@gmail.com>
2024-10-05 12:05:28 +0530

    fixed env type issue

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL as string;
+const apiUrl = (import.meta as any).env.VITE_API_URL as string;
+
 console.log(apiUrl);
 
 const axiosInstance = axios.create({
