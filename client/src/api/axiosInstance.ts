0aa87a65a3d34fbd1d49f3d07f7d018182a29729
Rahil <rahilsardar234@gmail.com>
2024-10-26 12:18:32 +0530

    changed the Server name to Server to Check

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -9,18 +9,20 @@ interface ImportMeta {
 }
 
 const apiUrl = (import.meta as unknown as ImportMeta).env.VITE_API_URL as string;
-const token = localStorage.getItem('token'); 
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
   },
 });
 
 axiosInstance.interceptors.request.use(
   config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   error => {
@@ -32,7 +34,19 @@ axiosInstance.interceptors.response.use(
   response => {
     return response;
   },
-  error => {
+  async error => {
+    const originalRequest = error.config;
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axiosInstance.get('/api/refreshToken');
+        const newToken = response.data.accessToken;
+        originalRequest.headers.Authorization = `Bearer ${newToken}`;
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
     return Promise.reject(error);
   }
 );
