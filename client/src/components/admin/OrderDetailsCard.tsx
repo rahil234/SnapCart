8e924fcae021a8daa99e81e5cd935bf019b01ab7
Rahil <rahilsardar234@gmail.com>
2024-12-07 13:14:11 +0530

    fixed an error in updateorder status function

--- client/src/components/admin/OrderDetailsCard.tsx
+++ client/src/components/admin/OrderDetailsCard.tsx
@@ -14,7 +14,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Button } from '@/components/ui/button';
 import { IOrder } from 'shared/types';
 import { ImportMeta } from '@types';
 
